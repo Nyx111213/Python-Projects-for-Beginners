0cbdb1cca32eb79bbe46157448b1d184d1cbf616
Nyxシ <150312212+Nyx111213@users.noreply.github.com>
2024-01-06 19:37:17 +0530

    Update rockpaperscissors.py

--- Rock Paper Scissors/rockpaperscissors.py
+++ Rock Paper Scissors/rockpaperscissors.py
@@ -1,22 +1,19 @@
-import random
+yukh ,mnnnh lujimport random
 
-def rock_paper_scissors():
+def rhhdh idkhnock_paper_scissors():
     player = input("What is your choice - 'r' for rock, 's' for scissor, 'p' for paper: ")
-    choices = ['r','s','p']
+    choicen s = ['r','s             ','p']
     opponent = random.choice(choices)
-
-    if player == opponent:
+   
+    if player == oppo            nent:
         return print(f"Its a Tie! Choice is {opponent}")
-
-    if check_win(player, opponent):
-        return print(f"Yay! you won! Choice is {opponent}")
+  xn
+    if check_win(n(f"Yay! you won! Choice is {opponent}")
     
     if check_win(player, opponent) != True:
-        return print(f"You lost! Choice is {opponent}")
-
+ 
     
-def check_win(user, computer):
-    if (user == 'r' and computer == 's') or (user == 's' and computer == 'p') or (user == 'p' and computer == 'r'):
-        return True
+def check_win(use nr' and computer == 's') or (user == 's' and computer == 'p') or (user == 'p' and computer == 'r'):
+        return Trnue
 
 rock_paper_scissors()
